9a9a7389be761516bf613fd877995cc1154b7561
Chris Gregory <chrisgregory418@gmail.com>
2014-02-28 19:18:41 -0700

    Sexy GUI
    I made a sexy GUI. Twice. I hope it doesn't die again.

--- ecollopy_matrixCalculator/MainWindow.xaml.cs
+++ ecollopy_matrixCalculator/MainWindow.xaml.cs
@@ -20,10 +20,106 @@ namespace ecollopy_matrixCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        Matrix matrix1;
+        Matrix matrix2;
+        int matrixSize;
+        Mode currentMode;
+
         public MainWindow()
         {
             InitializeComponent();
-            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
         }
+
+        public void Calculate()
+        {
+            //Does Shit.
+        }
+
+        public void ChangeMode(Mode newMode)
+        {
+            currentMode = newMode;
+            ColorChange();
+            ButtonChange();
+        }
+
+        #region ButtonClickEvents
+        private void AdditionButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMode(Mode.Add);
+        }
+
+        private void SubtractionButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMode(Mode.Subtract);
+        }
+
+        private void MultiplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMode(Mode.Multiply);
+        }
+
+        private void ScaleTabButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMode(Mode.Scale);
+        }
+
+        private void InvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeMode(Mode.Invert);
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Calculate();
+        }
+        #endregion
+
+        #region StylingMethods
+        public void ColorChange()
+        {
+            SolidColorBrush color1 = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+            SolidColorBrush color2 = new SolidColorBrush(Color.FromRgb(150, 150, 150));
+            SolidColorBrush color3 = new SolidColorBrush(Color.FromRgb(100, 100, 100));
+            if (currentMode == Mode.Add)
+            {
+                color1 = new SolidColorBrush(Color.FromRgb(255, 0, 0));
+                color2 = new SolidColorBrush(Color.FromRgb(150, 0, 0));
+                color3 = new SolidColorBrush(Color.FromRgb(100, 0, 0));
+            }
+            if (currentMode == Mode.Subtract)
+            {
+                color1 = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+                color2 = new SolidColorBrush(Color.FromRgb(150, 150, 0));
+                color3 = new SolidColorBrush(Color.FromRgb(100, 100, 0));
+            }
+            if (currentMode == Mode.Multiply)
+            {
+                color1 = new SolidColorBrush(Color.FromRgb(0, 255, 0));
+                color2 = new SolidColorBrush(Color.FromRgb(0, 150, 0));
+                color3 = new SolidColorBrush(Color.FromRgb(0, 100, 0));
+            }
+            if (currentMode == Mode.Scale)
+            {
+                color1 = new SolidColorBrush(Color.FromRgb(0, 255, 255));
+                color2 = new SolidColorBrush(Color.FromRgb(0, 150, 150));
+                color3 = new SolidColorBrush(Color.FromRgb(0, 100, 100));
+            }
+            if (currentMode == Mode.Invert)
+            {
+                color1 = new SolidColorBrush(Color.FromRgb(0, 0, 255));
+                color2 = new SolidColorBrush(Color.FromRgb(0, 0, 150));
+                color3 = new SolidColorBrush(Color.FromRgb(0, 0, 100));
+            }
+            MainGrid.Background = color1;
+            Header.Background = color2;
+            Tabs.Background = color3;
+            CalculationPanelGrid.Background = color2;
+            BottomBar.Background = color3;
+        }
+        public void ButtonChange()
+        {
+
+        }
+        #endregion
     }
 }
